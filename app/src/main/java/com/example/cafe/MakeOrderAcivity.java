eac225c5c9e645e4e086ee9d960e306fe4386858
Utoben <ubelousov15@gmail.com>
2024-01-10 15:40:45 +0500

    init Cafe interface

---
+++ app/src/main/java/com/example/cafe/MakeOrderAcivity.java
@@ -0,0 +1,14 @@
+package com.example.cafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeOrderAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_order_acivity);
+    }
+}
