c72e241b34aa63c2edc7bb535f97ef4630c3021b
Utoben <ubelousov15@gmail.com>
2024-01-11 16:33:27 +0500

    feat: new intent, post data to second

--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -4,14 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonSignin;
+    private String username;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, MakeOrderAcivity.class);
-        startActivity(intent);
+
+        initViews();
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = editTextName.getText().toString().trim();
+                password = editTextPassword.getText().toString().trim();
+
+                if (username.isEmpty() || password.isEmpty()){
+                    Log.d("MainActivity", "Empty field/s");
+                    Toast.makeText(
+                            MainActivity.this,
+//                            getString(R.string.fill_the_fields),
+                            R.string.fill_the_fields,
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    launchNextScreen(username);
+                }
+            }
+        });
     }
+    private  void launchNextScreen(String userName){
+        Log.d("MainActivity", "Username: " + userName);
+        Log.d("MainActivity", "Password: " + password);
+
+        Intent intent = MakeOrderAcivity.newIntent(this, userName);
+        startActivity(intent);
+//        Intent intent = new Intent(MainActivity.this, MakeOrderAcivity.class);
+//        intent.putExtra("userName", userName);
+    }
+
+    private  void initViews(){
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignin = findViewById(R.id.buttonSignin);
+    }
+
 }
--- app/src/main/java/com/example/cafe/MakeOrderAcivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderAcivity.java
@@ -2,13 +2,57 @@ package com.example.cafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MakeOrderAcivity extends AppCompatActivity {
-
+    private static final String EXTRA_USER_NAME = "userName";
+    private TextView textViewGreetings;
+    private TextView textViewAddittives;
+    private Button buttonMakeOrder;
+    private RadioGroup radioGroupDrinks;
+    private RadioButton radioButtonTea;
+    private RadioButton radioButtonCoffe;
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxLemon;
+    private Spinner tea;
+    private Spinner coffee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order_acivity);
+        initViews();
+
+        String userName = getIntent().getStringExtra(EXTRA_USER_NAME);
+        String greetings = getString(R.string.greetings);
+        String result = String.format(greetings, userName);
+    }
+    public static Intent newIntent(Context context, String userName){
+        Intent intent = new Intent(context, MakeOrderAcivity.class);
+        intent.putExtra(EXTRA_USER_NAME, userName);
+        return  intent;
+    }
+    private  void initViews(){
+        textViewGreetings = findViewById(R.id.textViewGreetings);
+        textViewAddittives = findViewById(R.id.textViewAddittives);
+        buttonMakeOrder = findViewById(R.id.buttonMakeOrder);
+        radioGroupDrinks = findViewById(R.id.radioGroupDrinks);
+        radioButtonTea = findViewById(R.id.radioButtonTea);
+        radioButtonCoffe = findViewById(R.id.radioButtonCoffe);
+        checkBoxSugar = findViewById(R.id.checkBoxSugar);
+        checkBoxMilk = findViewById(R.id.checkBoxMilk);
+        checkBoxLemon = findViewById(R.id.checkBoxLemon);
+        tea = findViewById(R.id.spinnerTea);
+        coffee = findViewById(R.id.spinnerCoffee);
+
     }
 }
