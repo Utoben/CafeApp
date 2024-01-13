399509bea0760c9c07913b036d6c3625bec62442
Utoben <ubelousov15@gmail.com>
2024-01-13 21:10:39 +0500

    feat: add notifications and permissions request

--- app/src/main/java/com/example/cafe/MakeOrderAcivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderAcivity.java
@@ -1,9 +1,19 @@
 package com.example.cafe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -32,6 +42,14 @@ public class MakeOrderAcivity extends AppCompatActivity {
     private Spinner coffee;
     private String drink;
     private String userName;
+    // Идентификатор уведомления
+    private static int NOTIFY_ID = 101;
+
+    // Идентификатор канала
+    private static final String CHANNEL_ID = "Cat channel";
+    private static final int REQUEST_CODE_SEND_NOTIFICATIONS = 123;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,9 +63,9 @@ public class MakeOrderAcivity extends AppCompatActivity {
         radioGroupDrinks.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int id) {
-                if(id == radioButtonTea.getId()){
+                if (id == radioButtonTea.getId()) {
                     onUserChooseTea();
-                } else if(id == radioButtonCoffe.getId()){
+                } else if (id == radioButtonCoffe.getId()) {
                     onUserChooseCoffee();
                 }
 
@@ -55,15 +73,77 @@ public class MakeOrderAcivity extends AppCompatActivity {
         });
         radioButtonTea.setChecked(true);
         buttonMakeOrder.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 onUserMadeOrder();
+
+//                // Проверка разрешения
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.)
+//                            != PackageManager.PERMISSION_GRANTED) {
+//                        // Запрос разрешения
+//                        ActivityCompat.requestPermissions(this,
+//                                new String[]{Manifest.permission.SEND_NOTIFICATIONS},
+//                                REQUEST_CODE_SEND_NOTIFICATIONS);
+//                    } else {
+//                        // Разрешение уже предоставлено, выполнение вашей логики
+//                        // Например, инициализация NotificationManager и отправка уведомления
+//                    }
+//                } else {
+//                    // В случае, если версия SDK не требует динамического запроса разрешений
+//                    // Разрешение считается предоставленным
+//                    // Выполнение вашей логики
+//                }
+
+
+                if (ContextCompat.checkSelfPermission(MakeOrderAcivity.this, "SEND_NOTIFICATIONS")
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Запрос разрешения
+                    ActivityCompat.requestPermissions(MakeOrderAcivity.this,
+                            new String[]{"SEND_NOTIFICATIONS"},
+                            REQUEST_CODE_SEND_NOTIFICATIONS);
+                }
+
+                initializeNotificationSending();
+                // или
+                //with(NotificationManagerCompat.from(this)) {
+                //    notify(NOTIFICATION_ID, builder.build()) // посылаем уведомление
+                //}
             }
         });
 
 
 //        String result = String.format(greetings, userName);
 //        textViewGreetings.setText(result);
+    }
+
+    private void initializeNotificationSending() {
+        NotificationChannel channel = new NotificationChannel("Cat channel", "Cat Channel", NotificationManager.IMPORTANCE_DEFAULT);//                        NotificationManagerCompat.from(MakeOrderAcivity.this);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MakeOrderAcivity.this);
+        notificationManager.createNotificationChannel(channel);
+        // Создаём уведомление
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(MakeOrderAcivity.this, "Cat channel")
+                        .setSmallIcon(R.drawable.lc_cafe_scaled)
+                        .setContentTitle("Сообщение")
+                        .setContentText("Ваш заказ принят")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+//            ActivityCompat.requestPermissions(this,
+//                                new String[]{Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION},
+//                                REQUEST_CODE_SEND_NOTIFICATIONS);
+            
+        }
+        notificationManager.notify(NOTIFY_ID, builder.build());
+        Log.d("MakeOrderAcivity", "ZII");
     }
 
     private void onUserMadeOrder(){
